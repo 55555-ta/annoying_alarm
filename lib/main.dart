00b661acb87bc235cb4d0c2beea277bfd0437fd1
(55555ta) <stiker3615@gmail.com>
2024-02-14 10:46:57 +0900

    main.dart gitignore test

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,12 @@ import 'package:annoying_alarm/alarmRinging_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Scaffold(
-      body: AlarmRingingScreen(),
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: AlarmRingingScreen(),
+        backgroundColor: Colors.amber,
+      ),
     ),
-  ));
+  );
 }
