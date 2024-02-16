4477d63ec1fd257b37773ae705be6572877f984a
kwj <david14722@gmail.com>
2024-02-16 17:59:44 +0900

    벨소리 선택 컨테이너 클릭시 배경색 변경 및 리스트뷰완성

--- lib/screens/alarmsound_screen.dart
+++ lib/screens/alarmsound_screen.dart
@@ -1,16 +1,24 @@
-import 'package:annoying_alarm/widgets/alarmsound_button.dart';
 import 'package:flutter/material.dart';
+import 'package:annoying_alarm/widgets/alarmsound_tiltebox.dart';
+import 'package:annoying_alarm/widgets/alarmsound_button.dart';
+import 'package:annoying_alarm/widgets/alarmsound_bell.dart';
 
-class AlarmsoundScreen extends StatelessWidget {
-  const AlarmsoundScreen({super.key});
+class AlarmsoundScreen extends StatefulWidget {
+  const AlarmsoundScreen({Key? key}) : super(key: key);
+
+  @override
+  _AlarmsoundScreenState createState() => _AlarmsoundScreenState();
+}
+
+class _AlarmsoundScreenState extends State<AlarmsoundScreen> {
+  bool _isBellSoundVisible = false;
+  bool _isChecked = false;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        const SizedBox(
-          height: 60,
-        ),
+        const SizedBox(height: 60),
         const Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -24,93 +32,27 @@ class AlarmsoundScreen extends StatelessWidget {
             ),
           ],
         ),
-        const SizedBox(
-          height: 40,
-        ),
+        const SizedBox(height: 40),
         const Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             AlarmsoundButton(), // switch기능 및 클릭시 배경색 바뀌는 기능
           ],
         ),
-        const SizedBox(
-          height: 20,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              width: 300.0,
-              height: 70.0,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(25.0),
-                color: const Color(0xff0c8e3f7),
-              ),
-              child: const Padding(
-                padding: EdgeInsets.symmetric(
-                  vertical: 12,
-                  horizontal: 30,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '벚꽃엔딩1', // 사운드선택 데이터값부분
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Text(
-                      '벚꽃엔딩',
-                      style: TextStyle(
-                        fontSize: 10,
-                        fontWeight: FontWeight.w600,
-                        color: Color(0xff006ACB),
-                      ),
-                    ), // 사운드선택 데이터값부분2
-                  ],
-                ),
-              ),
-            ),
-          ],
+        const SizedBox(height: 20),
+        GestureDetector(
+          onTap: () {
+            setState(() {
+              _isChecked = !_isChecked;
+              _isBellSoundVisible = !_isBellSoundVisible;
+            });
+          },
+          child: alarmsound_tiltebox(_isChecked),
         ),
         const SizedBox(
           height: 35,
         ),
-        Row(
-          children: [
-            Container(
-              width: 392.7,
-              height: 325.0,
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(25.0),
-                  topRight: Radius.circular(25.0),
-                ),
-                color: Color(0xffE8E8E8),
-              ),
-              child: const Padding(
-                padding: EdgeInsets.symmetric(
-                  vertical: 20,
-                  horizontal: 30,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '벨소리',
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
+        BellSoundContainer(isVisible: _isBellSoundVisible),
       ],
     );
   }
---
+++ lib/widgets/alarmsound_bell.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class BellSoundContainer extends StatefulWidget {
+  final bool isVisible;
+
+  const BellSoundContainer({Key? key, required this.isVisible})
+      : super(key: key);
+
+  @override
+  _BellSoundContainerState createState() => _BellSoundContainerState();
+}
+
+class _BellSoundContainerState extends State<BellSoundContainer> {
+  @override
+  Widget build(BuildContext context) {
+    const int itemCount = 5;
+    return AnimatedOpacity(
+      duration: const Duration(milliseconds: 500),
+      opacity: widget.isVisible ? 1.0 : 0.0,
+      child: Visibility(
+        visible: widget.isVisible,
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 500),
+          height: widget.isVisible ? 325.0 : 0,
+          width: 392.7,
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(25.0),
+              topRight: Radius.circular(25.0),
+            ),
+            color: Color(0xffE8E8E8),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 20,
+              horizontal: 30,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  '벨소리',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    physics: const AlwaysScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: itemCount,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text('벚꽃엔딩 ${index + 1}'),
+                        // 필요에 따라 추가적인 위젯을 구성
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
