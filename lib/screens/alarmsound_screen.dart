c53a214a32128e7c5c89f4af6e3bfb514ad8f12c
kwj <david14722@gmail.com>
2024-02-21 15:38:09 +0900

    푸터 작성중...커밋

--- lib/screens/alarmsound_screen.dart
+++ lib/screens/alarmsound_screen.dart
@@ -1,4 +1,3 @@
-import 'package:annoying_alarm/widgets/alarmsound_footer.dart';
 import 'package:flutter/material.dart';
 import 'package:annoying_alarm/widgets/alarmsound_tiltebox.dart';
 import 'package:annoying_alarm/widgets/alarmsound_button.dart';
@@ -14,58 +13,51 @@ class AlarmsoundScreen extends StatefulWidget {
 class _AlarmsoundScreenState extends State<AlarmsoundScreen> {
   bool _isBellSoundVisible = false;
   bool _isChecked = false;
-    int _selectedIndex = 0; // 추가
-
-  void _onItemTapped(int index) { // 추가
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
+  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const SizedBox(height: 60),
-        const Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Column(
-              children: [
-                Text(
-                  '사운드 설정',
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600),
-                ),
-              ],
-            ),
-          ],
-        ),
-        const SizedBox(height: 40),
-        const Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            AlarmsoundButton(), // switch기능 및 클릭시 배경색 바뀌는 기능
-          ],
-        ),
-        const SizedBox(height: 20),
-        GestureDetector(
-          onTap: () {
-            setState(() {
-              _isChecked = !_isChecked;
-              _isBellSoundVisible = !_isBellSoundVisible;
-            });
-          },
-          child: alarmsound_tiltebox(_isChecked),
-        ),
-        const SizedBox(
-          height: 35,
-        ),
-        BellSoundContainer(isVisible: _isBellSoundVisible),
-        const alarmsound_footer(),
-      ],
-    ),
-   bottomNavigationBar: BottomNavigationBar(
+    return Scaffold(
+      body: Column(
+        children: [
+          const SizedBox(height: 60),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  Text(
+                    '사운드 설정',
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(height: 40),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AlarmsoundButton(), // switch기능 및 클릭시 배경색 바뀌는 기능
+            ],
+          ),
+          const SizedBox(height: 20),
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                _isChecked = !_isChecked;
+                _isBellSoundVisible = !_isBellSoundVisible;
+              });
+            },
+            child: alarmsound_tiltebox(_isChecked),
+          ),
+          const SizedBox(
+            height: 35,
+          ),
+          BellSoundContainer(isVisible: _isBellSoundVisible),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
@@ -75,14 +67,20 @@ class _AlarmsoundScreenState extends State<AlarmsoundScreen> {
             icon: Icon(Icons.business),
             label: 'Business',
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
-          ),
         ],
+        // 선택된 아이템 색상 설정
+        unselectedItemColor: const Color(0xffC8E3F7),
+        selectedItemColor: Colors.white,
+        backgroundColor: const Color(0xff008FE0),
+        // 현재 선택된 인덱스
         currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
+        // 아이템을 클릭했을 때 호출되는 콜백 함수
+        onTap: (index) {
+          // TODO: 아이템을 클릭했을 때의 동작 구현
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
       ),
     );
   }
